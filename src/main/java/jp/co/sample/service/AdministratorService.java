9bc594dbb05db77ca382b1d17e8c12e22fd6b709
koichi.nagata <koichi.nagata@rakus.co.jp>
2019-05-28 14:00:07 +0900

    管理者リポジトリから登録用のメソッドの呼び出しをするメソッドの作成。

--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.repository.AdministratorRepository;
 
 /**
@@ -17,4 +18,13 @@ public class AdministratorService {
 	
 	@Autowired
 	private AdministratorRepository administratorRepository;
+	
+	/**
+	 * 管理者情報を登録するメソッド.
+	 * @param administrator　管理者情報を受け取る
+	 * 管理者情報のリポジトリから、登録用のメソッドを呼び出す
+	 */
+	public void insert(Administrator administrator) {
+		administratorRepository.insert(administrator);
+	}
 }
