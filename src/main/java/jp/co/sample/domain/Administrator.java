8f864a734d2361afad714d3588f064ea6f5701ed
koichi.nagata <koichi.nagata@rakus.co.jp>
2019-05-28 10:32:39 +0900

    管理者のドメイン作成

---
+++ src/main/java/jp/co/sample/domain/Administrator.java
@@ -0,0 +1,67 @@
+package jp.co.sample.domain;
+
+
+/**
+ * 管理者情報を表すドメイン
+ * 
+ * @author user
+ *
+ */
+public class Administrator {
+	
+	/** ID */
+	private Integer id;
+	/**　名前　*/
+	private String name;
+	/** メールアドレス  */
+	private String mailAddress;
+	/** パスワード */
+	private String password;
+	
+	public Administrator() {
+		
+	}
+	
+	public Administrator(Integer id, String name, String mailAddress, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.mailAddress = mailAddress;
+		this.password = password;
+	}
+
+
+
+	public final Integer getId() {
+		return id;
+	}
+	public final void setId(Integer id) {
+		this.id = id;
+	}
+	public final String getName() {
+		return name;
+	}
+	public final void setName(String name) {
+		this.name = name;
+	}
+	public final String getMailAddress() {
+		return mailAddress;
+	}
+	public final void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+	public final String getPassword() {
+		return password;
+	}
+	public final void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "Administrator [id=" + id + ", name=" + name + ", mailAddress=" + mailAddress + ", password=" + password
+				+ "]";
+	}
+	
+	
+}
